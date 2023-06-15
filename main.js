8a5e3ab3372ae25f0f23d86f9477bc0871b8a641
JehiraXavier <jehiraxavier@gmail.com>
2023-06-15 11:49:46 +0530

    Code cleaning

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 var options = {
-    strings: ["A Frontend Web Developer...", "A Freelancer...", "An Avid Reader...",],
-    typeSpeed: 50,
-    backDelay: 500,
-    fadeOut: true,
-    // smartBackspace: true, // this is a default
-    loop: true
-}
-var typed = new Typed('#designation', options);
+  strings: [
+    "A Frontend Web Developer...",
+    "A Freelancer...",
+    "An Avid Reader...",
+  ],
+  typeSpeed: 50,
+  backDelay: 500,
+  fadeOut: true,
+  loop: true,
+};
+var typed = new Typed("#designation", options);
